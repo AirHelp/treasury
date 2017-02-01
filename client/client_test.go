1b1943d090a72b35b25f43d162125b1c560971fe
Krzysztof Szarek <kszarek@gmail.com>
2017-02-01 14:06:07 +0100

    Revert "Move test consts in proper place"
    This reverts commit 67d86064b5c4fe8a1a91494386ff950ba5c53951.

--- client/client_test.go
+++ client/client_test.go
@@ -6,6 +6,11 @@ import (
 	"github.com/AirHelp/treasury/client"
 )
 
+const (
+	testKey    = "test/webapp/cocpit_api_pass"
+	testSecret = "as9@#$%^&*(/2hdiwnf"
+)
+
 func TestClient(t *testing.T) {
 	_, err := client.New("testBucketName", &client.Options{})
 	if err != nil {
